04cd9b8016d17a839921bb207b826e6810065137
Duncan M <sirduncan887@gmail.com>
2025-10-01 10:26:34 +0300

    Delete users/mpesa_callback.php

--- users/mpesa_callback.php
+++
@@ -1,6 +0,0 @@
-<?php
-$data = file_get_contents("php://input");
-file_put_contents("callback.json", $data . PHP_EOL, FILE_APPEND);
-
-http_response_code(200);
-echo json_encode(["ResultCode" => 0, "ResultDesc" => "Callback received successfully"]);
