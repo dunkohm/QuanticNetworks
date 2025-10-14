0c79ec94c371abda868cf33bea5c8f223d334544
Duncan M <sirduncan887@gmail.com>
2025-10-14 05:55:08 +0300

    Delete configs directory

--- configs/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'consumer_key'    => 'lY1ByZCKF9UuFRczrGRiaGPtC611nwoEko3fq5pszW0DLwjU',
-    'consumer_secret' => 'vallXLP89eCqGAFY10JvGpe5UpzsSfvGwMVe7AnXLAAlsSKAsB3MOZ2xOZTpMh6N',
-    'shortcode'       => '174379',
-    'passkey'         => 'npol/QjLyyZtRCq7xlhy0gKaLPqTDgAn00l17bVzmcfsnfij8XnvEYMXqrZc0kYOLGtmHh+8DwizoNG80iwMa4bvaD3gBSJ7OmSsQTFpBT6CZSiFkoi9qO+YqAZBW0yOCzGHVLCWAm6pTvPJf7jxamzqgFqQNfmnDXKHrvB1q+zOSY6bcbD+m0c9EOdWmpu7MqUjzQZhr78SEMJsf/FyOoNOYqhRvDiITWbp78uTHE2LpJhbN0Tsz4YfZyU/u+c2DtqDqTByP6HTlF70GOk3KUE6/e0kczCH6wFvOXiZzHLVERH2coGxlZV576rXKN/x0LP8/vdzEqzRW8KWTbHTCw==',
-    'callback_url'    => 'https://4aa767e207d0.ngrok-free.app/Quantic_Networks/users/mpesa_callback.php'
-];
-//C:\xampp\htdocs\Quantic Networks\configs\config.php
